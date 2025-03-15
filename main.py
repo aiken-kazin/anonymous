59ce0ceb6658e4b07ebddde8f804e6a68515194b
aiken.kazin <aikenkazin@gmail.com>
2025-03-15 23:29:50 +0500

    Add main py

--- main.py
+++ main.py
@@ -1,51 +1,35 @@
 import streamlit as st
-
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import pandas as pd
+from google.oauth2.service_account import Credentials
 
+# Читаем учетные данные из Streamlit Secrets
+import json
+credentials_info = json.loads(st.secrets["GOOGLE_CREDENTIALS"])
 
+# Авторизация с Google Sheets API
+credentials = Credentials.from_service_account_info(credentials_info, scopes=[
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+])
 
-# Specify path to your file with credentials
-path_to_credential = 'project2-453818-894de48f5133.json' 
-
-# Specify name of table in google sheets
-table_name = 'table'
-
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
-
-credentials = ServiceAccountCredentials.from_json_keyfile_name(path_to_credential, scope)
 gs = gspread.authorize(credentials)
 
+# Открываем Google Sheet
+table_name = "table"
 work_sheet = gs.open(table_name)
 sheet1 = work_sheet.sheet1
 
-# data = sheet1.get_all_values()
-# headers = data.pop(0)
-
-# df = pd.DataFrame(data, columns=headers)
-# print(df.head())
-
-
-
-
-
 def save_text(text):
-    """Сохраняет введенный текст в файл."""
-    new_row = [text]
-    sheet1.append_row(new_row)
+    """Сохраняет введенный текст в Google Sheets."""
+    sheet1.append_row([text])
 
-# Заголовок приложения
+# UI Streamlit
 st.title("Напишите анонимное сообщение")
-
-# Поле для ввода текста
 user_input = st.text_area("Введите текст:")
 
-# Кнопка для отправки
-if st.button("Отправит анонимное сообщение"):
-    if user_input.strip():  # Проверяем, что текст не пустой
+if st.button("Отправить анонимное сообщение"):
+    if user_input.strip():
         save_text(user_input)
         st.success("Текст успешно отправлен!")
     else:
-        st.warning("Введите текст перед отправлением.")
+        st.warning("Введите текст перед отправкой.")
